b05a222341a92884f1c410e6f330367668404fa2
frescogamba <gordo@yugo.at>
2014-12-30 13:37:55 +0100

    upload.sh - filename can be in subfolder

--- software/upload.sh
+++ software/upload.sh
@@ -11,5 +11,10 @@ fi
 # copy file via scp
 scp $FILE root@$HOST:
 
+if [[ "$FILE" == *\/* ]] || [[ "$FILE" == *\\* ]]
+then
+  FILE=`basename $FILE`
+fi
+# echo $FILE
 # reset arduino and upload with avrdude
 ssh root@$HOST "stty -F /dev/ttyUSB0 hupcl |  avrdude -c stk500v1 -b57600 -P /dev/ttyUSB0 -v -p m328p -u -U flash:w:$FILE"
