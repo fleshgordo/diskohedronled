974e2f7aa7cd2b2e678e14f8419e817970ac649b
frescogamba <gordo@yugo.at>
2014-12-30 13:44:28 +0100

    added shutdown script beaglebone

---
+++ software/shutdown.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+HOST=192.168.66.1
+
+ssh root@$HOST "shutdown -h now"
--- software/upload.sh
+++ software/upload.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 FILE=$1
-HOST=10.42.0.56
+#HOST=10.42.0.56
+HOST=192.168.66.1
 
 if [ "$#" -lt "1" ]; then
     echo "No hex file provided. Usage: ./upload.sh filename.hex "
@@ -17,4 +18,4 @@ then
 fi
 # echo $FILE
 # reset arduino and upload with avrdude
-ssh root@$HOST "stty -F /dev/ttyUSB0 hupcl |  avrdude -c stk500v1 -b57600 -P /dev/ttyUSB0 -v -p m328p -u -U flash:w:$FILE"
+ssh root@$HOST "stty -F /dev/ttyUSB0 hupcl |  avrdude -c stk500v1 -b57600 -P /dev/ttyUSB0 -p m328p -u -U flash:w:$FILE"
