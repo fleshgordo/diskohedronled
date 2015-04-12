739bab79cc1cdd3b122e3dd82c5f69b9cb445a32
frescogamba <gordo@yugo.at>
2015-04-12 14:45:23 +0200

    after biel

--- software/beaglebone/master.py
+++ software/beaglebone/master.py
@@ -3,11 +3,15 @@ import serial, time, os
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 output=0
 while True:
-	time.sleep(11)
-	print "writing 1 to white, 0 to black"
-	ser.write("0")
-	os.system("ssh pi@192.168.66.10 \"echo 0 > /tmp/fifo\"")
-	time.sleep(11)
-	print "writing 0 to white, 1 to black"
-	ser.write("1")
-	os.system("ssh pi@192.168.66.10 \"echo 1 > /tmp/fifo\"")
+	try:
+			print "writing 0 to white, 0 to black"
+					ser.write("0")
+							time.sleep(11)
+			os.system("ssh pi@192.168.66.10 \"echo 0 > /tmp/fifo\"")
+					print "writing 1 to white, 1 to black"
+							ser.write("1")
+									time.sleep(11)
+			os.system("ssh pi@192.168.66.10 \"echo 1 > /tmp/fifo\"")
+				except:
+						print "error"
+								time.sleep(2)
