4144d73f446fbcdc23cbd695a26d3f1e052696da
jazoza <selena@kucjica.org>
2016-01-10 22:30:13 +0100

    adding doc for software and medium-done new presets for sinusRGBW....)

--- software/www/js/presets.js
+++ software/www/js/presets.js
@@ -1,42 +1,74 @@
 var presets = {
-	"1": {
-		"pName": "wait",
-		"id": "1",
-		"entries": {
-			"full": "165",
-			"verylow": "0"
-		}
-	},
-	"2": {
-		"pName": "colornoise",
-		"id": "2",
-		"entries": {
-			"KRAZYRED": [165, 0, 0, 0],
-			"KRAZYGREEN": [0, 165, 0, 0],
-			"KRAZYBLUE": [0, 0, 165, 0],
-			"redred": [165, 165, 160, 161]
-		}
-	},
-	"3": {
-		"pName": "GOL",
-		"id": "3",
-		"entries": {
-			"preset": [165, 200],
-			"zero": [1, 2]
-		}
-	},
-	"4": {
-		"pName": "boot",
-		"id": "4",
-		"entries": {
-			"fastboot": [165, 200]
-		}
-	},
-	"5": {
-		"pName": "RGBW",
-		"id": "5",
-		"entries": {
-			"KRAZYRED": [165, 0, 0, 0],
-		}
-	}
+    "1": {
+        "pName": "wait",
+        "id": "1",
+        "entries": {
+            "full": "165",
+            "verylow": "3",
+            "cue3": "4",
+            "CUE_DARK": "0"
+        }
+    },
+    "2": {
+        "pName": "colornoise",
+        "id": "2",
+        "entries": {
+            "KRAZYRED": [165, 0, 0, 0],
+            "KRAZYGREEN": [0, 165, 0, 0],
+            "KRAZYBLUE": [0, 0, 165, 0],
+            "redred": [165, 165, 160, 161]
+        }
+    },
+    "3": {
+        "pName": "GOL",
+        "id": "3",
+        "entries": {
+            "preset": [180, 165],
+            "zero": [1, 2],
+            "cue1": [29, 10],
+            "cue3": [10, 29],
+            "CUE1": [106, 109],
+            "CUE2": [171, 178],
+            "CUE_DARK": [149, 106]
+        }
+    },
+    "4": {
+        "pName": "boot",
+        "id": "4",
+        "entries": {
+            "fastboot": [165, 200]
+        }
+    },
+    "5": {
+        "pName": "RGBW",
+        "id": "5",
+        "entries": {
+            "KRAZYRED": [165, 0, 0, 0]
+        }
+    },
+    "101": {
+        "pName": "sinusRed",
+        "id": "6",
+        "entries": {}
+    },
+    "102": {
+        "pName": "sinusBlue",
+        "id": "7",
+        "entries": {}
+    },
+    "103": {
+        "pName": "sinusGreen",
+        "id": "8",
+        "entries": {}
+    },
+    "104": {
+        "pName": "sinusWhite",
+        "id": "9",
+        "entries": {}
+    },
+    "105": {
+        "pName": "sinusAll",
+        "id": "10",
+        "entries": {}
+    }
 };
