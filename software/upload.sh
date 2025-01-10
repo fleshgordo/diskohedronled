faa81740e0a0ad21f5d2d288005d197371a772c7
Gordan <robot@yugo.at>
2025-01-10 15:14:47 +0100

    fixing firmware

--- software/upload.sh
+++ software/upload.sh
@@ -2,8 +2,8 @@
 
 FILE=$1
 #HOST=10.42.0.56
-HOST=192.168.66.1
-PORT=/dev/ttyACM0
+HOST=192.168.178.113
+PORT=/dev/ttyUSB0
 
 if [ "$#" -lt "1" ]; then
     echo "No hex file provided. Usage: ./upload.sh filename.hex "
@@ -19,4 +19,4 @@ then
 fi
 # echo $FILE
 # reset arduino and upload with avrdude
-ssh root@$HOST "stty -F $PORT hupcl && avrdude -c stk500v1 -b57600 -P $PORT -p m328p -u -U flash:w:$FILE"
+ssh root@$HOST "stty -F $PORT hupcl && avrdude -v -c stk500v1 -b57600 -P $PORT -p atmega328p -U flash:w:$FILE"
